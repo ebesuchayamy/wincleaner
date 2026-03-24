ff0956d28994053e2e32e104863acde6af9f6433
Fidgaizy <ebesuchayamyjxk@hotmail.com>
2026-03-25 02:44:54 +0300

    Add Win11 Optimizer PowerShell GUI script

---
+++ win11_optimizer.ps1
@@ -0,0 +1,11 @@
+# PowerShell script to optimize Windows 11
+
+# Function to add an optimized Windows 11 experience
+Function Optimize-Windows11 {
+    # This is where the optimization code will go.
+    Write-Host 'Optimizing Windows 11...'
+    # Add your optimization commands here.
+}
+
+# Call the optimization function
+Optimize-Windows11
